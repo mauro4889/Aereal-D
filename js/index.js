d110f3f9c9bd59499b1b3600b1dd12c875bc8a48
Mauro Spinello <mauro4889@gmail.com>
2022-07-09 23:30:12 -0300

    Se agrego contador me gusta

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 let inicio = document.getElementById('inicio');
 let nosotros = document.getElementById('sobreNosotros');
 let contacto = document.getElementById('contacto');
+let mgBosque = document.getElementById('mgBosque');
+let mgMar = document.getElementById('mgMar');
+let mgEventos = document.getElementById('mgEventos');
+let contadorBosque = document.getElementById('contadorBosque');
+let contadorMar = document.getElementById('contadorMar');
+let contadorEventos = document.getElementById('contadorEvento');
+let valorBosque = 0;
+let valorMar = 0;
+let valorEvento = 0;
+let valor = { bosque: 0, mar: 0, evento: 0 };
 
 /* MENU HAMBURGUESA */
 document.querySelector('.btn_menu').addEventListener('click', animateBars);
@@ -18,6 +28,8 @@ function animateBars() {
 }
 /* FIN MENU HAMBURGUESA */
 
+/* PAGINA ACTUAL */
+
 let urlActual = window.location.pathname
 console.log(urlActual.pathname);
 
@@ -27,7 +39,43 @@ const paginaActual = (pagina) => {
     }
 }
 
+/*FIN PAGINA ACTUAL */
+
+/*CONTADOR MG */
+
+mgBosque.addEventListener('click', () => {
+    valorBosque = valor.bosque + 1;
+    valor.bosque = valorBosque;
+    contadorBosque.textContent = valorBosque;
+    localStorage.setItem('contador', JSON.stringify(valor))
+})
+
+
+
+mgMar.addEventListener('click', () => {
+    valorMar = valor.mar + 1;
+    valor.mar = valorMar;
+    contadorMar.textContent = valorMar;
+    localStorage.setItem('contador', JSON.stringify(valor))
+})
+
+mgEventos.addEventListener('click', () => {
+    valorEvento = valor.evento + 1;
+    valor.evento = valorEvento;
+    contadorEventos.textContent = valorEvento;
+    localStorage.setItem('contador', JSON.stringify(valor))
+})
+
+
+
+
 window.addEventListener('DOMContentLoaded', (e) => {
     paginaActual(urlActual);
-    checkPosition();
+    let valorData = JSON.parse(localStorage.getItem('contador'));
+    valor.bosque = valorData.bosque;
+    valor.mar = valorData.mar;
+    valor.evento = valorData.evento;
+    contadorBosque.textContent = valor.bosque;
+    contadorMar.textContent = valor.mar;
+    contadorEventos.textContent = valor.evento;
 })
